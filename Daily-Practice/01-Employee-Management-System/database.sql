d59dbfafa2b446e975a0690afb5a72c1a1be46e1
namrata217 <118385346+namrata217@users.noreply.github.com>
2026-09-25 10:12:20 +0530

    Create employee table

--- Daily-Practice/01-Employee-Management-System/database.sql
+++ Daily-Practice/01-Employee-Management-System/database.sql
@@ -1,2 +1,33 @@
+/*
+    Project: Employee Management System
+    Database: MySQL 8.0
+    Purpose: SQL learning and hands-on practice
+*/
+
+-- ============================================
+-- 1. CREATE DATABASE
+-- ============================================
+
+
 CREATE DATABASE employee_management;
+
+-- Select the database
 USE employee_management;
+
+
+-- ============================================
+-- 2. CREATE EMPLOYEE TABLE
+-- ============================================
+
+CREATE TABLE employee
+(   
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL, 
+    email varchar(100) UNIQUE, 
+    age INT CHECK(age>=18),
+    department VARCHAR(50) NOT NULL, 
+    salary INT,
+    joining_date DATE
+  );
+
+
